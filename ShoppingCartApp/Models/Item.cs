4eed1751a6dd5395ac26072ab68a9234576112de
adam <adamsatran@gmail.com>
2026-01-30 23:14:35 +0200

    Fixed Item Class

--- ShoppingCartApp/Models/Item.cs
+++ ShoppingCartApp/Models/Item.cs
@@ -1,11 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-public class Item
+namespace ShoppingCartApp.Models
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public decimal Price { get; set; }
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
 
-    [Range(1, 100, ErrorMessage = "The field Quantity must be between 1 and 100")]
-    public int Quantity { get; set; }
+        [Range(1, 100, ErrorMessage = "The field Quantity must be between 1 and 100")]
+        public int Quantity { get; set; }
+    }
 }
